a036d2589f8a110df75db3dfd27ebeb0a994a2fb
Gustavo Ignacio <125836120+GustavoIgn@users.noreply.github.com>
2024-03-20 14:35:15 -0300

    Update Controller.java

--- Ex_3.4_ED/src/controller/Controller.java
+++ Ex_3.4_ED/src/controller/Controller.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controller;
 
 public class Controller {
@@ -19,25 +18,3 @@ public class Controller {
 		}
 	}
 }
-=======
-package controller;
-
-public class Controller {
-	public Controller() {
-		super();
-	}
-
-	public String ConverterBinario(int n) {
-		// Condição de Parada: Após as divisões N se encontra como 0, logo devo retornar
-		// colhendo os restos das divisões anteriores.
-		if (n <= 0) {
-			return "";
-		} else {
-			// A cada retorno eu concateno o valor do resto da divisão de n por 2, de forma
-			// inversa, ou seja, na ordem de binário e passo o resultado da divisão de n por
-			// 2 para a proxima volta.
-			return String.valueOf(ConverterBinario(n / 2) + n % 2);
-		}
-	}
-}
->>>>>>> bd1900993ee0abac65dd55f79e04e36af486857e
