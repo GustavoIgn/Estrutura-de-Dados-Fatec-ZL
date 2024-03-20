ffb22fc9900836d97752d972254f44a1d5650c88
Gustavo Ignacio <125836120+GustavoIgn@users.noreply.github.com>
2024-03-20 14:39:15 -0300

    Update Controller.java

--- Ex_3.6_ED/src/Controller/Controller.java
+++ Ex_3.6_ED/src/Controller/Controller.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Controller;
 
 public class Controller {
@@ -26,32 +25,3 @@ public class Controller {
 		}
 	}
 }
-=======
-package Controller;
-
-public class Controller {
-	public Controller() {
-		super();
-	}
-
-	public Double calcFatorial(int n) {
-		// Condição de Parada: n = 1, retorna o fatorial, no caso 1.
-		if (n == 1) {
-			return 1.0;
-		} else {
-			// Multiplica o valor de N pelo seu antecessor, até 1.
-			return n * calcFatorial(n - 1);
-		}
-	}
-
-	public Double calcSoma(int n) {
-		// Condição de Parada: Quando n for 1, retorna 1 para a soma da série.
-		if (n == 1) {
-			return 1.0;
-		} else {
-			// A cada volta é somado fatorial com n e seus antecessores.
-			return 1 / calcFatorial(n) + calcSoma(n - 1);
-		}
-	}
-}
->>>>>>> bd1900993ee0abac65dd55f79e04e36af486857e
