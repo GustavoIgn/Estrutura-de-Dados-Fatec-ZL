16115a6a51e40448b10b64877dff5aca341de142
Gustavo Ignacio <125836120+GustavoIgn@users.noreply.github.com>
2024-03-20 14:30:56 -0300

    Update View.java

--- Ex_3.2_ED/src/view/View.java
+++ Ex_3.2_ED/src/view/View.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-package view;
-
-import javax.swing.JOptionPane;
-
-import controller.Controller;
-
-public class View {
-	public static void main(String[] args) {
-		Controller cont = new Controller();
-		boolean valido = false;
-		
-		while (valido != true) {
-			int n1 = Integer.parseInt(JOptionPane.showInputDialog("Insira um valor de 10 a 9999999"));
-			int n2 = Integer.parseInt(JOptionPane.showInputDialog("Insira um valor de 0 a 10"));
-			
-			if ((n1 >= 10 && n1 <= 999999) && (n2 >= 0 && n2 <= 10)) {
-				valido = true;
-				System.out.println("O número " + n2 + " aparece " + cont.RetornaVezes(n1, n2) + " vez(es) em " + n1);
-			} else {
-				valido = false;
-			}
-		}
-		
-		
-	}
-}
-=======
 package view;
 
 import java.util.Random;
@@ -55,4 +27,3 @@ public class View {
 		
 	}
 }
->>>>>>> bd1900993ee0abac65dd55f79e04e36af486857e
