42f5586f792d4e81a3de9c87c0ab00d2b431044d
Gustavo Ignacio <125836120+GustavoIgn@users.noreply.github.com>
2024-03-20 14:43:29 -0300

    Update Controller.java

--- Ex_4.1_ED_QuickSort/src/controller/Controller.java
+++ Ex_4.1_ED_QuickSort/src/controller/Controller.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controller;
 
 import br.edu.fateczl.ordenacao.MetodosOrdenacao;
@@ -17,23 +16,3 @@ public class Controller {
 		}
 	}
 }
-=======
-package controller;
-
-import br.edu.fateczl.ordenacao.MetodosOrdenacao;
-
-public class Controller {
-	public Controller() {
-		super();
-	}
-
-	public void ordenacao(int[] vt) {
-		MetodosOrdenacao ord = new MetodosOrdenacao();
-		vt = ord.quickSort(vt, 0, vt.length - 1);
-		System.out.println("\nVetor Ordenado: ");
-		for (int i : vt) {
-			System.out.print(i + " ");
-		}
-	}
-}
->>>>>>> bd1900993ee0abac65dd55f79e04e36af486857e
