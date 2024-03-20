858f659aa13ed2450d7edf8eee734367fd1186a5
Gustavo Ignacio <125836120+GustavoIgn@users.noreply.github.com>
2024-03-20 14:28:24 -0300

    Update Controller.java

--- Ex_3.1_ED/src/controller/Controller.java
+++ Ex_3.1_ED/src/controller/Controller.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controller;
 
 public class Controller {
@@ -16,22 +15,3 @@ public class Controller {
 			}
 	}
 }
-=======
-package controller;
-
-public class Controller {
-	public Controller() {
-		super();
-	}
-	
-	public int RetornaDigitos(int n) {
-		// Condição de Parada: Número com apenas um digito.
-			if (n < 10) {
-				return 1;
-			} else {
-				//Número com pelo menos dois digitos, então somo 1 e faço a recursiva para o próximo digito.
-				return 1 + RetornaDigitos(n/10);
-			}
-	}
-}
->>>>>>> bd1900993ee0abac65dd55f79e04e36af486857e
