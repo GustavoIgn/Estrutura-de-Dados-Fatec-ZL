e74538a39e80008c0eb8b9316866f17ce05369a0
Gustavo Ignacio <125836120+GustavoIgn@users.noreply.github.com>
2024-03-20 14:33:49 -0300

    Update View.java

--- Ex_3.3_ED/src/view/View.java
+++ Ex_3.3_ED/src/view/View.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package view;
 
 import javax.swing.JOptionPane;
@@ -15,21 +14,3 @@ public class View {
 	}
 
 }
-=======
-package view;
-
-import javax.swing.JOptionPane;
-import controller.Controller;
-public class View {
-
-	public static void main(String[] args) {
-		Controller cont = new Controller();
-		String texto = JOptionPane.showInputDialog("Insira a cadeia de caracteres");
-		int tamanho = texto.length();
-		
-		System.out.println("Invertendo " + texto + " fica " + cont.InverterTexto(texto, tamanho));
-
-	}
-
-}
->>>>>>> bd1900993ee0abac65dd55f79e04e36af486857e
