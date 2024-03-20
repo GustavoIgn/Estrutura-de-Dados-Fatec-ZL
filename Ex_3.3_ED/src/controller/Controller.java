04a80bbd5e537a668b52389feaac33608183d6da
Gustavo Ignacio <125836120+GustavoIgn@users.noreply.github.com>
2024-03-20 14:31:52 -0300

    Update Controller.java

--- Ex_3.3_ED/src/controller/Controller.java
+++ Ex_3.3_ED/src/controller/Controller.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controller;
 
 public class Controller {
@@ -20,26 +19,3 @@ public class Controller {
 		}
 	}
 }
-=======
-package controller;
-
-public class Controller {
-	public Controller() {
-		super();
-	}
-
-	public String InverterTexto(String txt, int tamanho) {
-		// Condição de Parada: o tamanho da palavra ser igual a 0, significa que a
-		// palavra é inexistente/acabou e retorna vazio.
-		if (tamanho == 0) {
-			return "";
-		} else {
-			String l = txt.substring(0, 1);
-			tamanho = tamanho - 1;
-			return InverterTexto(txt.substring(1), tamanho) + l;
-			// A cada volta a String l pega a primeira letra da palavra, e quando retorna
-			// ela retorna com uma letra a menor
-		}
-	}
-}
->>>>>>> bd1900993ee0abac65dd55f79e04e36af486857e
