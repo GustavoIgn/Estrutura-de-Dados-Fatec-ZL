0d8c9a86dc0292f80478630cff25848e653ec896
Gustavo Ignacio <125836120+GustavoIgn@users.noreply.github.com>
2024-03-20 14:38:01 -0300

    Update View.java

--- Ex_3.5_ED/src/view/View.java
+++ Ex_3.5_ED/src/view/View.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package view;
 
 import javax.swing.JOptionPane;
@@ -23,29 +22,3 @@ public class View {
 	}
 
 }
-=======
-package view;
-
-import javax.swing.JOptionPane;
-
-import controller.Controller;
-
-public class View {
-
-	public static void main(String[] args) {
-		Controller cont = new Controller();
-		boolean valido = false;
-
-		while (valido != true) {
-			int n = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor de 0 a 20"));
-			if (n > 0 && n <= 20) {
-				valido = true;
-				System.out.println("O termo " + n + " da serie de Fibonacci é: " + cont.calcFibonacci(n));
-			} else {
-				valido = false;
-			}
-		}
-	}
-
-}
->>>>>>> bd1900993ee0abac65dd55f79e04e36af486857e
